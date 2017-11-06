e7fe7f3434bd2ea8880bb94f003db376a8408fad
Sahana <sahana@example.com>
2017-11-06 12:51:20 +0400

    commit 1:skeleton of the app

---
+++ app/src/main/java/com/example/android/cvcwordproject/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.cvcwordproject;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("Word Quiz");
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitleTextColor(Color.WHITE
+        );
+        setSupportActionBar(myToolbar);
+      //set up listner for play image,if clicked start the quiz
+        ImageView play_image=(ImageView) findViewById(R.id.play_image);
+        play_image.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+               Intent intent=new Intent(MainActivity.this,Quiz.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
